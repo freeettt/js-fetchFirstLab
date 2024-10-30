a7e27cd7395778ff637adfd3e911cf004946bf8d
freeettt <122921308+freeettt@users.noreply.github.com>
2024-10-30 23:20:55 +0200

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,12 +1,43 @@
-"Ваш код повинен зробити PATCH-запит до вказаного URL, де {userId} – це ID існуючого користувача."
-"Для оновлення користувача передайте в запит нові дані, наприклад, нове ім’я."
-"Поверніть відповідь від сервера з оновленими даними користувача."
-
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
+const https = require('https');
 
 function updateUser(id, updatedData) {
-  // Ваш код
+    const data = JSON.stringify(updatedData);
+
+    const options = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: /users/${id},
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            let responseData = '';
+
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const response = JSON.parse(responseData);
+                    resolve(response);
+                } catch (error) {
+                    reject(error);  
+                }
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error);  
+        });
+
+        req.write(data);
+        req.end();
+    });
 }
 
 console.log(updateUser(1, { name: 'New Name' }));
