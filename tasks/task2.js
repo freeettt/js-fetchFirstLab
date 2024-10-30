f5bccfd50e51cfa20f5e2e9125ca1accae396292
freeettt <122921308+freeettt@users.noreply.github.com>
2024-10-30 23:20:32 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,14 +1,43 @@
-"Ваш код повинен зробити POST-запит до вказаного URL."
-"Для створення нового користувача, передайте в запит наступні дані:"
-"name: ваше ім’я"
-"email: ваш email"
-"Поверніть відповідь від сервера після створення користувача."
-
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
+const https = require('https');
 
 function createUser(user) {
-  // Ваш код
+    const data = JSON.stringify(user);
+
+    const options = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: '/users',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            let responseData = '';
+
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const response = JSON.parse(responseData);
+                    resolve(response);
+                } catch (error) {
+                    reject(error);  
+                }
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error);  
+        });
+
+        req.write(data);
+        req.end();
+    });
 }
 
 console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
